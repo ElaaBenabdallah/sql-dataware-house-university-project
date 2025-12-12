b9c6c6b220577fe271d8c723ef5f56cc834dec88
Elaa Benabdallah <ella.benabdallah004@gmail.com>
2025-12-12 13:36:11 +0100

    Update proc_load_bronze.sql

--- bronze/proc_load_bronze.sql
+++ bronze/proc_load_bronze.sql
@@ -1,34 +1,64 @@
-TRUNCATE TABLE bronze.sales_teams;
-BULK INSERT bronze.sales_teams
-FROM 'C:\Users\USER\Desktop\career\CRM+Sales+Opportunities\sales_teams.csv'
+TRUNCATE TABLE bronze.fashion_store_campaigns;
+BULK INSERT bronze.fashion_store_campaigns
+FROM 'C:\Users\USER\Desktop\career\fashion store\dataset_fashion_store_campaigns.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
     TABLOCK );
 
-TRUNCATE TABLE bronze.products;
-BULK INSERT bronze.products
-FROM 'C:\Users\USER\Desktop\career\CRM+Sales+Opportunities\products.csv'
+TRUNCATE TABLE bronze.fashion_store_channels;
+BULK INSERT bronze.fashion_store_channels
+FROM 'C:\Users\USER\Desktop\career\fashion store\dataset_fashion_store_channels.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
     TABLOCK );
 
-TRUNCATE TABLE bronze.accounts;
-BULK INSERT bronze.accounts
-FROM 'C:\Users\USER\Desktop\career\CRM+Sales+Opportunities\accounts.csv'
+ALTER TABLE bronze.fashion_store_customers
+DROP COLUMN revenue
+
+
+TRUNCATE TABLE bronze.fashion_store_customers;
+BULK INSERT bronze.fashion_store_customers
+FROM 'C:\Users\USER\Desktop\career\fashion store\dataset_fashion_store_customers.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
     TABLOCK );
 
-ALTER TABLE bronze.sales_pipeline
-ALTER COLUMN opportunity_id NVARCHAR(50);
 
-TRUNCATE TABLE bronze.sales_pipeline;
-BULK INSERT bronze.sales_pipeline
-FROM 'C:\Users\USER\Desktop\career\CRM+Sales+Opportunities\sales_pipeline.csv'
+TRUNCATE TABLE bronze.fashion_store_products;
+BULK INSERT bronze.fashion_store_products
+FROM 'C:\Users\USER\Desktop\career\fashion store\dataset_fashion_store_products.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
     TABLOCK );
+
+TRUNCATE TABLE bronze.fashion_store_sales;
+BULK INSERT bronze.fashion_store_sales
+FROM 'C:\Users\USER\Desktop\career\fashion store\dataset_fashion_store_sales.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+    TABLOCK );
+
+ALTER TABLE bronze.fashion_store_salesitems
+ALTER COLUMN discount_applied DECIMAL(18,2)
+
+TRUNCATE TABLE bronze.fashion_store_salesitems;
+BULK INSERT bronze.fashion_store_salesitems
+FROM 'C:\Users\USER\Desktop\career\fashion store\dataset_fashion_store_salesitems.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+    TABLOCK );
+
+TRUNCATE TABLE bronze.fashion_store_stock;
+BULK INSERT bronze.fashion_store_stock
+FROM 'C:\Users\USER\Desktop\career\fashion store\dataset_fashion_store_stock.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+    TABLOCK );
+
